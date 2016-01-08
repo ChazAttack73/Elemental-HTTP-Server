ef4ba6a826c56ab9d0c851d5c1edc6cb6a3d8b2d
Chaz <cmtlum73@gmail.com>
2016-01-07 16:58:59 -1000

    working on validating POST request to require 4 property keys

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var reqDate = new Date();
 var server = http.createServer( connectionMade ).listen( 8080 );
 
 function connectionMade( req, res ) {
-  //console.log( req );
-  //console.log( res );
 
   if( req.method === 'GET' ) {
     if( req.url === '/' ) {
@@ -39,26 +37,32 @@ function connectionMade( req, res ) {
       req.setEncoding( 'utf8' );
       req.on( 'data', function( chunk ) {
         var parsedChunk = querystring.parse( chunk );
-        //console.log( parsedChunk );
 
-        fs.readFile( 'public/element-template.html', function( err, data ) {
-          if ( err ) console.log( err );
-            var stringyTemplate = data.toString();
-            //console.log( stringyTemplate );
+//====================== VALIDATE POST REQUEST FORMAT
+        // if( parsedChunk.hasOwnProperty( 'elementName' ) === false ) {
+        //   res.writeHead( 400, {
+        //     'Success': 'False',
+        //     'Message': 'Please enter elementName as key in body of request'
+        //   });
+        //   res.end();
+        // }
 
-            var newElement = stringyTemplate
-              .replace( '{{NEW NAME HERE}}', parsedChunk.elementName )
-              .replace( '{{NEW ELEMENT NAME}}', parsedChunk.elementName )
-              .replace( '{{NEW ELEMENT SYMBOL}}', parsedChunk.elementSymbol )
-              .replace( '{{NEW ELEMENT ATOMIC NUMBER}}', parsedChunk.elementAtomicNumber )
-              .replace( '{{NEW ELEMENT DESCRIPTION}}', parsedChunk.elementDescription );
-              //console.log( newElement );
+          fs.readFile( 'public/element-template.html', function( err, data ) {
+            if ( err ) console.log( err );
+              var stringyTemplate = data.toString();
+
+              var newElement = stringyTemplate
+                .replace( '{{NEW NAME HERE}}', parsedChunk.elementName )
+                .replace( '{{NEW ELEMENT NAME}}', parsedChunk.elementName )
+                .replace( '{{NEW ELEMENT SYMBOL}}', parsedChunk.elementSymbol )
+                .replace( '{{NEW ELEMENT ATOMIC NUMBER}}', parsedChunk.elementAtomicNumber )
+                .replace( '{{NEW ELEMENT DESCRIPTION}}', parsedChunk.elementDescription );
 
 
-            fs.writeFile( 'public/' + parsedChunk.elementName + '.html', newElement, function( err ) {
-              if ( err ) console.log( err );
-            });
-        });
+              fs.writeFile( 'public/' + parsedChunk.elementName.toLowerCase() + '.html', newElement, function( err ) {
+                if ( err ) console.log( err );
+              });
+          });
       });
     }
     res.end();
