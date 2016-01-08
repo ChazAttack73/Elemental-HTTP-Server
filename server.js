d599f7ce9f546bf987735aade0fde995e085b3e3
Chaz <cmtlum73@gmail.com>
2016-01-07 15:56:21 -1000

    POST creates or modifies new file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require( 'http' );
 var fs = require( 'fs' );
+var querystring = require( 'querystring');
 var reqDate = new Date();
 
 
@@ -33,5 +34,33 @@ function connectionMade( req, res ) {
     });
   }
 
-  res.end( 'hello you' );
+  if( req.method === 'POST' ) {
+    if( req.url === '/elements' ) {
+      req.setEncoding( 'utf8' );
+      req.on( 'data', function( chunk ) {
+        var parsedChunk = querystring.parse( chunk );
+        //console.log( parsedChunk );
+
+        fs.readFile( 'public/element-template.html', function( err, data ) {
+          if ( err ) console.log( err );
+            var stringyTemplate = data.toString();
+            //console.log( stringyTemplate );
+
+            var newElement = stringyTemplate
+              .replace( '{{NEW NAME HERE}}', parsedChunk.elementName )
+              .replace( '{{NEW ELEMENT NAME}}', parsedChunk.elementName )
+              .replace( '{{NEW ELEMENT SYMBOL}}', parsedChunk.elementSymbol )
+              .replace( '{{NEW ELEMENT ATOMIC NUMBER}}', parsedChunk.elementAtomicNumber )
+              .replace( '{{NEW ELEMENT DESCRIPTION}}', parsedChunk.elementDescription );
+              //console.log( newElement );
+
+
+            fs.writeFile( 'public/' + parsedChunk.elementName + '.html', newElement, function( err ) {
+              if ( err ) console.log( err );
+            });
+        });
+      });
+    }
+    res.end();
+  }
 }
